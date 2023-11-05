dce4ebccbddaca30fd72fcf0b755af76d103796b
HEBAMamdooh <heba.mamdooh975@gmail.com>
2023-11-05 12:05:16 -0800

    update files permissions

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,15 +9,16 @@
  */
 size_t list_int_len(const list_int_t *h)
 {
-    int len = 0;
+	int len = 0;
 
-    while (h != NULL)
-    {
-        ++len;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		++len;
 
-    return (len);
+		h = h->next;
+	}
+	
+	return (len);
 }
 
 /**
@@ -30,22 +31,20 @@ size_t list_int_len(const list_int_t *h)
  */
 list_int_t *get_node_int_at_index(list_int_t *head, unsigned int index)
 {
-    list_int_t *current = head;
-    unsigned int iter_times = 0;
+	list_int_t *current = head;
+	unsigned int iter_times = 0;
 
-    if (head)
-    {
-        while (current != NULL)
-        {
-            if (iter_times == index)
-                return (current);
-
-            current = current->next;
-            ++iter_times;
-        }
-    }
-
-    return (NULL);
+	if (head)
+	{
+		while (current != NULL)
+		{
+			if (iter_times == index)
+				return (current);
+			current = current->next;
+			++iter_times;
+		}
+	}
+	return (NULL);
 }
 
 /**
@@ -58,28 +57,27 @@ list_int_t *get_node_int_at_index(list_int_t *head, unsigned int index)
 
 int is_palindrome(list_int_t **head)
 {
-    list_int_t *start = NULL, *end = NULL;
-    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+	list_int_t *start = NULL, *end = NULL;
+	unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
 
-    if (head == NULL)
-        return (0);
+	if (head == NULL)
+		return (0);
+	if (*head == NULL)
+		return (1);
+	start = *head;
+	len = list_int_len(start);
+	len_cyc = len * 2;
+	list = len_cyc - 2;
+	end = *head;
 
-    if (*head == NULL)
-        return (1);
-
-    start = *head;
-    len = list_int_len(start);
-    len_cyc = len * 2;
-    len_list = len_cyc - 2;
-    end = *head;
-
-    for (; i < len_cyc; i = i + 2)
-    {
-        if (start[i].n != end[len_list].n)
-            return (0);
-
-        len_list = len_list - 2;
-    }
-
-    return (1);
+	for (; i < len_cyc; i = i + 2)
+	{
+		if (start[i].n != end[len_list].n)
+			
+			return (0);
+		
+		len_list = len_list - 2;
+	}
+	
+	return (1);
 }
