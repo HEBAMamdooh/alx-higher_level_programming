1ea52dfc4d84b9d7fbe1b5bbab31d1acd014ee64
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-01-17 20:31:27 +0200

    update answer

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,2 @@
--- Set the database name from the command-line argument
-USE `dbname`;
-
 -- Show all tables in the specified database
 SHOW TABLES;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,5 @@
--- Set the database name from the command-line argument
-USE `dbname`;
-
 -- Create the table if it does not exist
-CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS `first_table` (
     `id` INT,
-    name VARCHAR(256)
+    `name` VARCHAR(256)
 );
