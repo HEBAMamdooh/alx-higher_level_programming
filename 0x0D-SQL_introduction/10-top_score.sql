3ccbdf012e2782f4b1f8b59490ad08031d842885
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-01-17 21:26:35 +0200

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,6 +1,3 @@
--- Set the database name from the command-line argument
-USE `hbtn_0c_0`;
-
 -- List all records of the second_table ordered by score (top first)
 SELECT `score`, `name`
 FROM `second_table`
