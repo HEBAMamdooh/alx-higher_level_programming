972f714cd077a68614aa86cab842d4a801a7e695
abdallh <heba.mamdooh975@gmail.com>
2024-05-22 02:02:47 +0300

    JS - Web scraping - end of tasks

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const movieData = JSON.parse(body);
+    const charactersUrls = movieData.characters;
+
+    printCharacters(charactersUrls, 0);
+});
+
+function printCharacters(charactersUrls, index) {
+    if (index >= charactersUrls.length) {
+        return;
+    }
+
+    request(charactersUrls[index], (charError, charResponse, charBody) => {
+        if (charError) {
+            console.error(charError);
+            return;
+        }
+
+        const characterData = JSON.parse(charBody);
+        console.log(characterData.name);
+
+        printCharacters(charactersUrls, index + 1);
+    });
+}
---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const films = JSON.parse(body).results;
+    const wedgeMovies = films.filter(film =>
+        film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    );
+
+    console.log(wedgeMovies.length);
+});
